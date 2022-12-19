9efdd0d4a617fa34f1295a4cad9dc8b58b54f645
DV <unseedevil@gmail.com>
2022-12-19 15:56:27 +0300

    first commit

---
+++ Ex_01]/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("Ведите число А ");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine ("Введите число В ");
+int numberB = int.Parse(Console.ReadLine());
+double result = Math.Pow(numberB,2);
+if (numberA == result)
+{
+System.Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+else
+{
+  System.Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");  
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine ("Введите число A");
+int numberA = int.Parse (Console.ReadLine());
+double result = Math.Pow(numberA,2);
+System.Console.WriteLine($"Квадрат от числа {numberA} равен {result}");
